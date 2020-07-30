3befad37379a244cc8e59d9bade2864ace5bca78
Daniele Giuliani <daniele.giuliani@studenti.unitn.it>
2020-07-30 10:18:11 +0200

    Add instruction count limiter in order to terminate correctly the program

--- majid_display/fuzz.sh
+++ majid_display/fuzz.sh
@@ -4,6 +4,6 @@ DIR="$(dirname "$(readlink -f "$0")")"
 BINARY=./Display.yml
 INPUTS=./inputs
 OUTPUTS=./output/
-HARNESS="python3 -m hal_fuzz.harness -c $BINARY -l 0"
-./../afl-fuzz -U -m none -t 10000 -i $INPUTS -o $OUTPUTS -- $HARNESS @@
+HARNESS="python3 -m hal_fuzz.harness -c $BINARY -l 4000000"
+./../afl-fuzz -U -m none -t 4000 -i $INPUTS -o $OUTPUTS -- $HARNESS @@
 #$HARNESS $INPUTS/STLogo.bmp
--- majid_display/fuzz_paraller_resume.sh
+++ majid_display/fuzz_paraller_resume.sh
@@ -3,11 +3,11 @@
 BINARY=./Display.yml
 INPUTS=-
 OUTPUTS=./output/
-HARNESS="python3 -m hal_fuzz.harness -c $BINARY"
+HARNESS="python3 -m hal_fuzz.harness -c $BINARY -l 4000000"
 nprocs=22
 for i in `seq 2 $nprocs`; do
-    ./../afl-fuzz -t 10000 -S slave$i -U -m none -i $INPUTS -o $OUTPUTS -- $HARNESS @@ >/dev/null 2>&1 &
+    ./../afl-fuzz -t 4000 -S slave$i -U -m none -i $INPUTS -o $OUTPUTS -- $HARNESS @@ >/dev/null 2>&1 &
 done
-./../afl-fuzz -t 10000+ -M master -U -m none -i $INPUTS -o $OUTPUTS -- $HARNESS @@
+./../afl-fuzz -t 4000+ -M master -U -m none -i $INPUTS -o $OUTPUTS -- $HARNESS @@
 pkill afl-fuzz
 
