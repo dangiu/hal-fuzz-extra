79900033973a643c7cb4cd5125936a9cf3829864
Daniele Giuliani <daniele.giuliani@studenti.unitn.it>
2020-08-15 12:14:06 +0200

    Add new test case for instrumented version of Display firmware

---
+++ majid_display_instrumented/fuzz.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1  # needed it we don't have root priviledges on the machine and don't care about missing some crashes
+
+DIR="$(dirname "$(readlink -f "$0")")"
+BINARY=./Display_Instrumented.yml
+INPUTS=./inputs
+OUTPUTS=./output/
+HARNESS="python3 -m hal_fuzz.harness -c $BINARY -l 4000000"
+./../afl-fuzz -U -m none -t 4000 -i $INPUTS -o $OUTPUTS -- $HARNESS @@
+#$HARNESS $INPUTS/STLogo.bmp
---
+++ majid_display_instrumented/fuzz_paraller_resume.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1  # needed it we don't have root priviledges on the machine and don't care about missing some crashes
+
+BINARY=./Display_Instrumented.yml
+INPUTS=-
+OUTPUTS=./output/
+HARNESS="python3 -m hal_fuzz.harness -c $BINARY -l 4000000"
+nprocs=22
+for i in `seq 2 $nprocs`; do
+    ./../afl-fuzz -t 4000 -S slave$i -U -m none -i $INPUTS -o $OUTPUTS -- $HARNESS @@ >/dev/null 2>&1 &
+done
+./../afl-fuzz -t 4000+ -M master -U -m none -i $INPUTS -o $OUTPUTS -- $HARNESS @@
+pkill afl-fuzz
+
